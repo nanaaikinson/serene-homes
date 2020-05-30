f501a9e6883920f90a2976ff72b61bd2a13addb3
Nana Kwesi Ofosu-Aikins <nanaaikinson24@gmail.com>
2020-05-30 09:51:45 +0000

    Test production

--- resources/admin/src/router/index.js
+++ resources/admin/src/router/index.js
@@ -44,7 +44,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  base:
+    process.env.NODE_ENV === "production" ? "/admin/" : process.env.BASE_URL,
   routes
 });
 
--- resources/admin/vue.config.js
+++ resources/admin/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   devServer: {
     proxy: "http://localhost:8000"
-  }
+  },
+  outputDir: "../../public/assets/admin",
+
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/assets/admin/" : "/admin",
+
+  // modify the location of the generated HTML file.
+  indexPath:
+    process.env.NODE_ENV === "production"
+      ? "../../../resources/views/pages/admin.blade.php"
+      : "index.html"
 };
